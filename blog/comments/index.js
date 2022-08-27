a5382bcdc7a882bba36a501ae85f742fc9731d6a
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-08-27 12:10:21 +0300

    blog: Completed React App

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,10 +1,12 @@
 const expores = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = expores();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
