10a330bc3b627867fad644e89bbd00ca2936e1f0
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-08-30 12:59:39 +0300

    blog: A Basic Event Bus Implementation

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
@@ -14,12 +15,25 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   posts[id] = { id, title };
 
   res.status(201).send(posts[id]);
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title,
+    },
+  });
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Events', req.body.type);
+  res.send({});
 });
 
 app.listen('4000', () => {
